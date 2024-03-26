c6ab8148018dec6800e4c8a001125cd459261caf
Janvier <uwiragiyejanvi@gmail.com>
2024-03-26 19:32:54 -0400

    blog page boilerplate

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">blog</h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        I write about software development, productivity, and other topics that
+        interest me. Check back soon for new posts! 👨‍💻
+      </p>
+    </>
+  );
+}
