5d8c55890f1b67012ab792a896f23162b467e605
Janvier <uwiragiyejanvi@gmail.com>
2024-03-26 18:31:40 -0400

    boilerplate

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import React from 'react';
+
+type NavItem = {
+  name: string;
+  path: string;
+};
+
+type NavItemProps = {
+  item: NavItem;
+  children?: React.ReactNode;
+};
+
+const navItemsList: NavItem[] = [
+  {
+    name: 'home',
+    path: '/',
+  },
+  {
+    name: 'work',
+    path: '/work',
+  },
+  {
+    name: 'blog',
+    path: '/blog',
+  },
+  {
+    name: 'ask ai',
+    path: '/ai',
+  },
+];
+
+export function Navbar() {
+  return (
+    <div className="menu pl-0 bg-base-200 menu-horizontal lg-horizontal mb-16 tracking-tight rounded-box">
+      {navItemsList.map((navItem) => (
+        <NavItem item={navItem} key={navItem.name} />
+      ))}
+    </div>
+  );
+}
+
+export function NavItem({ item, children }: NavItemProps) {
+  const { name } = item;
+  return (
+    <li>
+      <Link href={item.path}>{name}</Link>
+    </li>
+  );
+}
---
+++ src/app/work/page.tsx
@@ -0,0 +1,13 @@
+import { Tweet } from 'react-tweet';
+
+export default function WorkPage() {
+  return (
+    <>
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">work</h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        {`I'm currently working on a few projects that I'm excited to share with you soon. Stay tuned! 👨‍🍳`}
+      </p>
+      <Tweet id="1617979122625712128" />
+    </>
+  );
+}
