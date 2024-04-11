bff69d1b4e277480efed2ea4a7b05fa9422e180e
Janvier <uwiragiyejanvi@gmail.com>
2024-04-11 18:45:26 -0400

    add ai

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,7 +1,27 @@
-export default function Page() {
+'use client';
+import Markdown from 'react-markdown';
+
+import { useChat } from 'ai/react';
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tighter">ai</h1>
-    </section>
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map((m) => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          <Markdown>{m.content}</Markdown>
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
   );
 }
